36290fd9e116d1b706a0cb582b028e2bdd92050d
Raihan <raihan.dk@gmail.com>
2015-10-20 03:00:55 +0600

    Installed and configured Watch, Connect, and Bootstrap. Created related Grunt task for them.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,9 @@
 module.exports = function (grunt) {
 	// include required plugins
 	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-connect');
 	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	// task configuration
 	var taskConfig = {
@@ -18,6 +20,44 @@ module.exports = function (grunt) {
 			index: {
 				src: 'src/index.html',
 				dest: '<%= output_dir %>/index.html'
+			},
+			vendor_css: {
+				src: '<%= vendor_files.css %>',
+				dest: '<%= output_dir %>/'
+			},
+			vendor_js: {
+				src: '<%= vendor_files.js %>',
+				dest: '<%= output_dir %>/',
+				cwd: '.'
+			},
+			vendor_fonts: {
+				src: '<%= vendor_files.fonts %>',
+				dest: '<%= output_dir %>/',
+				cwd: '.'
+			},
+			vendor_assets: {
+				src: '<%= vendor_files.assets %>',
+				dest: '<%= output_dir %>/vendor/assets/',
+				cwd: '.'
+			}
+		},
+
+		// describe watch task
+		watching: {
+			files:['src/**/*', 'Gruntfile.js'],
+			tasks: ['build'],
+	        options: {
+	          	livereload: true
+	        }
+		},
+
+		// describe connect, the start of the app in a http server
+		connect: {
+			server: {
+				options: {
+					port: 9001,
+					base: '<%= output_dir %>'
+				}
 			}
 		}
 	};
@@ -27,6 +67,12 @@ module.exports = function (grunt) {
 
 	// initialize the configuration with taskConfig and userConfig
 	grunt.initConfig(grunt.util._.extend(taskConfig, userConfig));
+
+	// rename grunt watch task. because we want to run 'grunt watch' in order to build also
+	grunt.renameTask( 'watch', 'watching' );
+
+	// register watch task
+	grunt.registerTask('watch', ['build', 'connect', 'watching']);
 
 	// register build task
 	grunt.registerTask('build', ['clean', 'copy']);
--- grunt.user.config.js
+++ grunt.user.config.js
@@ -1,3 +1,21 @@
 module.exports = {
-	output_dir: 'dest'
+	output_dir: 'dist',
+
+	vendor_files: {
+	    js: [
+			'vendor/jquery/dist/jquery.min.js',
+			'vendor/bootstrap/dist/js/bootstrap.min.js'
+	    ],
+	    css: [
+	    	'vendor/bootstrap/dist/css/bootstrap.min.css'
+	    ],
+	    fonts: [
+	    	'vendor/bootstrap/dist/fonts/glyphicons-halflings-regular.eot',
+	    	'vendor/bootstrap/dist/fonts/glyphicons-halflings-regular.svg',
+	    	'vendor/bootstrap/dist/fonts/glyphicons-halflings-regular.ttf',
+	    	'vendor/bootstrap/dist/fonts/glyphicons-halflings-regular.woff',
+	    	'vendor/bootstrap/dist/fonts/glyphicons-halflings-regular.woff2'
+	    ],
+	    assets: []
+  	}
 };
