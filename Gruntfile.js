18cac4e361d134183591851110b701b83ffbd80c
Raihan <raihan.dk@gmail.com>
2015-10-19 17:41:47 +0600

    Configured Grunt and created Grunt task build.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,36 @@
 module.exports = function (grunt) {
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json')
-	});
+	// include required plugins
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+
+	// task configuration
+	var taskConfig = {
+		// load package data in pkg variable
+		pkg: grunt.file.readJSON('package.json'),
+
+		// describe clean task. this will delete the dest directory
+		clean: {
+			build: ['<%= output_dir %>']
+		},
+
+		// describe copy task
+		copy: {
+			index: {
+				src: 'src/index.html',
+				dest: '<%= output_dir %>/index.html'
+			}
+		}
+	};
+
+	// user configuration in an external file
+	var userConfig = require('./grunt.user.config.js');
+
+	// initialize the configuration with taskConfig and userConfig
+	grunt.initConfig(grunt.util._.extend(taskConfig, userConfig));
+
+	// register build task
+	grunt.registerTask('build', ['clean', 'copy']);
+
+	// register the default task
+	grunt.registerTask('default', ['build']);
 };
---
+++ grunt.user.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+	output_dir: 'dest'
+};
